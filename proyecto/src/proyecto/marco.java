26daab4bc471e4bef22a2d6c823dc292b2619966
mar1997 <33941989+mar1997@users.noreply.github.com>
2018-10-13 21:56:28 -0500

    Update marco.java

--- proyecto/src/proyecto/marco.java
+++ proyecto/src/proyecto/marco.java
@@ -11,7 +11,7 @@ package proyecto;
  */
 public class marco {
     public static void main(String[] args) {
-        System.out.println("marco");
+        System.out.println("marco jajajajja");
     }
             
 }
